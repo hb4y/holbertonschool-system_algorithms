7f5fda68409bf37635d91af67a67f50c07cf8c4f
hb4y <hugobaymon@gmail.com>
2020-10-08 01:37:20 +0000

    update header

--- 0x00-red_black_tree/rb_trees.h
+++ 0x00-red_black_tree/rb_trees.h
@@ -42,5 +42,6 @@ typedef struct rb_tree_s
 /* Prototypes */
 
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
+int rb_tree_is_valid(const rb_tree_t *tree);
 
 #endif /* _RB_TREES_H_ */
