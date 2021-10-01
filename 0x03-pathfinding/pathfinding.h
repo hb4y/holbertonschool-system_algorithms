8b32cdbba58163919280e51625d9e043c44fb492
hb4y <hugobaymon@gmail.com>
2021-09-30 22:27:12 -0500

    headers

---
+++ 0x03-pathfinding/pathfinding.h
@@ -0,0 +1,26 @@
+#ifndef _PATHFINDING_
+#define _PATHFINDING_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <math.h>
+#include "queues.h"
+#include "graphs.h"
+
+/**
+ * struct point_s - Structure storing coordinates
+ *
+ * @x: X coordinate
+ * @y: Y coordinate
+ */
+typedef struct point_s
+{
+	int x;
+	int y;
+} point_t;
+
+queue_t *backtracking_array(char **map, int rows, int cols,
+	point_t const *start, point_t const *target);
+#endif
