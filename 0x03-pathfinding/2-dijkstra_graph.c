9b18ee7bf701c1bbe0e2b004f4b66453fdf8efae
hb4y <hugobaymon@gmail.com>
2021-09-30 22:57:05 -0500

    betty

--- 0x03-pathfinding/2-dijkstra_graph.c
+++ 0x03-pathfinding/2-dijkstra_graph.c
@@ -28,18 +28,13 @@ queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 
 	if (!graph || !start || !target || !res)
 		return (NULL);
-
 	wei = calloc(graph->nb_vertices, sizeof(*wei));
 	org = calloc(graph->nb_vertices, sizeof(*org));
-
 	corn = calloc(graph->nb_vertices, sizeof(*corn));
-
 	if (!wei || !org || !corn || !res)
 		return (NULL);
-
 	for (ver = graph->vertices; ver; ver = ver->next)
 		corn[ver->index] = ver, wei[ver->index] = INT_MAX;
-
 	wei[start->index] = 0, org[start->index] = NULL;
 	while (k != (ssize_t)target->index)
 	{
@@ -48,10 +43,8 @@ queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 				f = wei[o], k = o;
 		if (k == -1)
 			break;
-
 		printf("Checking %s, distance org %s is %f\n",
 			corn[k]->content, start->content, wei[k]);
-
 		for (ed = corn[k]->edges; ed; ed = ed->next)
 			if (wei[ed->dest->index] >= 0 &&
 				wei[k] + ed->weight < wei[ed->dest->index])
@@ -59,16 +52,12 @@ queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 					org[ed->dest->index] = corn[k];
 		wei[k] = -1;
 	}
-
 	if (k != -1)
 		for (queue_push_front(res, DUP(corn[k]->content));
 			k != (ssize_t)start->index; k = org[k]->index)
 			queue_push_front(res, DUP(org[k]->content));
 	else
 		res = (free(res), NULL);
-
-	free(wei), free(org), free(corn);
-
-	return (res);
-
+free(wei), free(org), free(corn);
+return (res);
 }
