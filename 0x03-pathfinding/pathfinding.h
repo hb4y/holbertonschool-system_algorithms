b50c31fa536a89a68dfee5eb8860af4565f32c0a
hb4y <hugobaymon@gmail.com>
2021-09-30 23:16:28 -0500

    headers

--- 0x03-pathfinding/pathfinding.h
+++ 0x03-pathfinding/pathfinding.h
@@ -27,4 +27,6 @@ queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
 	vertex_t const *target);
 queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 	vertex_t const *target);
+queue_t *a_star_graph(graph_t *graph, vertex_t const *start,
+	vertex_t const *target);
 #endif
